f894e0925015b98aee98923778cffbce8eae5ae5
sehwanforeal <55143030+sehwanforeal@users.noreply.github.com>
2019-11-28 13:30:04 +0900

    first commit

---
+++ secode/group_meal_front/src/Main.js
@@ -0,0 +1,11 @@
+import React from "react";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "Styles/Main.scss";
+
+class Main extends React.Component {
+  render() {
+    return <></>;
+  }
+}
+
+export default Main;
