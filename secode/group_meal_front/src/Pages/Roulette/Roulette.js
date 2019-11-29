8bd00f1b082566bebaf9f704b82692107c0db3b9
sehwanforeal <55143030+sehwanforeal@users.noreply.github.com>
2019-11-29 14:07:13 +0900

    map 함수활용해 mockupdata 시각화

--- secode/group_meal_front/src/Pages/Roulette/Roulette.js
+++ secode/group_meal_front/src/Pages/Roulette/Roulette.js
@@ -1,14 +1,26 @@
 import React, { Component } from "react";
 import "./Roulette.scss";
 import Nav from "Components/Nav";
+import roulettemock from "Data/roulettemock.js";
+import GroupBox from "Components/GroupBox";
 
 export class Roulette extends Component {
+  constructor() {
+    super();
+    this.state = { rouletteMock: roulettemock };
+  }
   render() {
+    console.log(this.state);
     return (
       <div>
         <Nav />
         <div className="roulette-body">
-          <div className="rl-title">점술판</div>
+          <div className="rl-title">돌려돌려 점술판!!</div>
+          <div className="rl-result-container">
+            {this.state.rouletteMock.groupMeals.map((el, idx) => {
+              return <GroupBox info={el} index={idx} />;
+            })}
+          </div>
         </div>
       </div>
     );
