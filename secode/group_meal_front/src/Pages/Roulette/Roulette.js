948a425e767118dded9403a15923bd3c24140cb9
sehwanforeal <55143030+sehwanforeal@users.noreply.github.com>
2019-11-29 22:08:39 +0900

    map

--- secode/group_meal_front/src/Pages/Roulette/Roulette.js
+++ secode/group_meal_front/src/Pages/Roulette/Roulette.js
@@ -5,19 +5,61 @@ import roulettemock from "Data/roulettemock.js";
 import GroupBox from "Components/GroupBox";
 import PreGroupBox from "Components/PreGroupBox";
 import roulettegif from "Img/roulette.gif";
+import { thisExpression } from "@babel/types";
 
 export class Roulette extends Component {
   constructor() {
     super();
-    this.state = { rouletteMock: roulettemock, isClicked: false };
+    this.state = {
+      rouletteMockOriginal: roulettemock,
+      rouletteMockForUpdate: roulettemock,
+      isClicked: true
+    };
   }
+  identifierMethod = e => {
+    const currentInput = e.target.name;
+    console.log(currentInput);
+  };
+
   render() {
-    console.log(typeof true);
+    console.log(roulettemock.groupMeals[2].length);
+
+    let allInputs = [];
+    if (this.state.rouletteMockForUpdate) {
+      for (let i = 0; i < roulettemock.groupMeals.length; i++) {
+        for (let j = 0; j < roulettemock.groupMeals[i].length; j++) {
+          allInputs.push(
+            <input
+              name={`${i},${j}`}
+              value={roulettemock.groupMeals[i][j]}
+              onChange={this.identifierMethod}
+            />
+          );
+        }
+      }
+    }
     return (
       <div>
         <Nav />
         <div className="roulette-body">
           <div className="rl-empty"></div>
+
+          {/* {() => {
+            for (let i = 0; i < roulettemock.groupMeals.length; i++) {
+              for (let j = 0; j < roulettemock.groupMeals[i].length; j++) {
+                return (
+                  <input name={`${i}${j}`} onChange={this.identifierMethod} />
+                );
+              }
+            }
+          }} */}
+          {allInputs}
+
+          {/* {roulettemock.groupMeals.map((el1, idx1) => {
+            el1.map((el2, idx2) => {
+              return <input name={(idx1, idx2)} value={el2} />;
+            });
+          })} */}
 
           {this.state.isClicked ? (
             <div className="rl-result-container">
