bf85155c31cc11fd08926b3b28e2e255df26ebbc
Daniel Yoo <onikss793@gmail.com>
2019-12-01 21:22:34 +0900

    wip employees

--- secode/group_meal_front/src/Pages/Employeetable/Employeetable.js
+++ secode/group_meal_front/src/Pages/Employeetable/Employeetable.js
@@ -1,15 +1,80 @@
 import React, { Component } from "react";
-import "./Employeetable.scss";
+import "./EmployeeTable.scss";
 import Nav from "Components/Nav";
+import SideBar from "./SideBar";
+import Tables from "./Tables";
+import Modal from "./Modal";
 
-export class Employeetable extends Component {
+class EmployeeTable extends Component {
+  state = {
+    members: [],
+    memberInfo: {},
+    cells: [],
+    isModalOn: false
+  };
+
+  componentDidMount() {
+    this.fetchMembers();
+  }
+
+  fetchMembers = async () => {
+    const { members } = this.state;
+
+    const list = await fetch("http://localhost:3030/member").then(res =>
+      res.json()
+    );
+
+    this.setState({
+      members: members.concat(list)
+    });
+  };
+
+  handleClick = async name => {
+    const memberData = await fetch(
+      `http://localhost:3030/member/${name}`
+    ).then(res => res.json());
+    const cellData = await fetch(`http://localhost:3030/cell`).then(res =>
+      res.json()
+    );
+
+    this.setState({
+      memberInfo: memberData.member,
+      cells: cellData,
+      isModalOn: true
+    });
+  };
+
+  cancelModal = () => {
+    this.setState({ isModalOn: false });
+  };
+
   render() {
+    const { members, memberInfo, isModalOn, cells } = this.state;
+
     return (
-      <div>
-        <Nav />
-      </div>
+      <>
+        {isModalOn && (
+          <Modal
+            cells={cells}
+            cancelModal={this.cancelModal}
+            memberInfo={memberInfo}
+          />
+        )}
+        <div className="page">
+          <SideBar />
+          <main>
+            <div className="title">
+              <span>클둥이 목록</span>
+            </div>
+            <div className="tables">
+              <Tables onClick={this.handleClick} memberList={members} />
+            </div>
+          </main>
+          {/* <Nav /> */}
+        </div>
+      </>
     );
   }
 }
 
-export default Employeetable;
+export default EmployeeTable;
