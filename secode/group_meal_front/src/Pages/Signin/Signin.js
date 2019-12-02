022112936a56522ca2eac3d35624c7ee8b7e03b5
sehwanforeal <55143030+sehwanforeal@users.noreply.github.com>
2019-12-02 11:55:36 +0900

    wip

--- secode/group_meal_front/src/Pages/Signin/Signin.js
+++ secode/group_meal_front/src/Pages/Signin/Signin.js
@@ -1,9 +1,27 @@
 import React, { Component } from "react";
 import "./Signin.scss";
+import notfound from "Img/404.png";
 
 export class Signin extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
   render() {
-    return <div>인증인가</div>;
+    return (
+      <div className="signin-body">
+        <img className="signin-background" src={notfound} alt=""></img>
+        <a href="https://slack.com/oauth/authorize?scope=identity.basic,identity.email&client_id=854668779588.857346823878">
+          <img
+            alt="Sign in with Slack"
+            height="40"
+            width="172"
+            src="https://platform.slack-edge.com/img/sign_in_with_slack.png"
+            srcset="https://platform.slack-edge.com/img/sign_in_with_slack.png 1x, https://platform.slack-edge.com/img/sign_in_with_slack@2x.png 2x"
+          />
+        </a>
+      </div>
+    );
   }
 }
 
