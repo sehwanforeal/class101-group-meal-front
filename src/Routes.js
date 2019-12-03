c6d137268dcd7b9216544b287730505e1810afba
sehwanforeal <55143030+sehwanforeal@users.noreply.github.com>
2019-12-03 17:52:55 +0900

    secode deleted

---
+++ src/Routes.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Signin from "Pages/Signin";
+import AdminPage from "Pages/AdminPage";
+import EmployeeTable from "Pages/Employeetable";
+import Roulette from "Pages/Roulette";
+import Cell from "Pages/Cell";
+
+class Routes extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Signin} />
+          <Route exact path="/admin" component={AdminPage} />
+          <Route exact path="/employees" component={EmployeeTable} />
+          <Route exact path="/roulette" component={Roulette} />
+          <Route exact path="/cell" component={Cell} />
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+export default Routes;
