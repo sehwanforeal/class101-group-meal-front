d9ee68c509c620630e6069c7e836af25a2ed2955
onikss793 <53050493+onikss793@users.noreply.github.com>
2019-12-04 19:08:20 +0900

    Feature/deploy (#25)
    * url changed to backend deploy server

--- secode/group_meal_front/src/config.js
+++ secode/group_meal_front/src/config.js
@@ -2,4 +2,5 @@
 // exports.client_secret = "client_secret=50ca812413faf339cd2848f42b8762dd";
 exports.client_id = "client_id=2877224790.859064433381";
 exports.client_secret = "client_secret=a8a3140b7dd976601be6c52ffab797c2";
-exports.url = "http://10.0.2.62:3030/";
+exports.url =
+  " https://86ds5kfejd.execute-api.ap-northeast-2.amazonaws.com/prod/";
