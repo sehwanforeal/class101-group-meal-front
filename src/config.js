9d2b0f700e33e2e94d66ff73621fefad60a245d2
sehwanforeal <55143030+sehwanforeal@users.noreply.github.com>
2019-12-05 22:25:26 +0900

    config url changed

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@
 // exports.client_secret = "client_secret=50ca812413faf339cd2848f42b8762dd";
 exports.client_id = "client_id=2877224790.859064433381";
 exports.client_secret = "client_secret=a8a3140b7dd976601be6c52ffab797c2";
-exports.url = "http://54.180.92.87:3030/";
+exports.url = "https://ex-manager2.class101.net/";
 // "http://54.180.92.87:3030/";
 // " https://86ds5kfejd.execute-api.ap-northeast-2.amazonaws.com/prod/";
