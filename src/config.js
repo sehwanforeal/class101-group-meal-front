a78f9a4765021f044b646f0310fb2eb36d3f5bfd
한 <han@keullaeseu101s-MacBook-Pro.local>
2020-01-03 14:53:06 +0900

    endpoint ip changed

--- src/config.js
+++ src/config.js
@@ -17,16 +17,16 @@ exports.client_secret = "client_secret=781f7b365def4744355ff1d6a30d4fd1";
 // " https://86ds5kfejd.execute-api.ap-northeast-2.amazonaws.com/prod/";
 
 // http -------------------
-// exports.url = "https://ex-manager2.class101.net/";
-// exports.url = "https://ex-manager2.class101.net/"; // AWS
-// exports.url_j = "https://ex-manager2.class101.net/"; // 웅조
-// exports.url_q = "https://ex-manager2.class101.net/"; // 새글
-// exports.url_me = "http://localhost:3030/";
-
-exports.url = "http://10.58.5.167:3030/"; // AWS
-exports.url_j = "http://10.58.5.167:3030/"; // 웅조
-exports.url_q = "http://10.58.5.167:3030/"; // 새글
+exports.url = "https://ex-manager2.class101.net/";
+exports.url = "https://ex-manager2.class101.net/"; // AWS
+exports.url_j = "https://ex-manager2.class101.net/"; // 웅조
+exports.url_q = "https://ex-manager2.class101.net/"; // 새글
 exports.url_me = "http://localhost:3030/";
+
+// exports.url = "http://10.58.5.167:3030/"; // AWS
+// exports.url_j = "http://10.58.5.167:3030/"; // 웅조
+// exports.url_q = "http://10.58.5.167:3030/"; // 새글
+// exports.url_me = "http://localhost:3030/";
 
 // 10.58.2.197//wj
 // 10.58.6.107//sg
